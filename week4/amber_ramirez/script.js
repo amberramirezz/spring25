ecfc3583dbe725e5be96c1182f02295b86a46d7d
Amber Ramirez <amber.ramirez216@gmail.com>
2025-03-13 19:30:21 -0700

    changes

--- week4/amber_ramirez/script.js
+++ week4/amber_ramirez/script.js
@@ -1,3 +1,8 @@
+<img src="https://ih1.redbubble.net/image.1272486145.7615/st,small,507x507-pad,600x600,f8f8f8.jpg" onclick="addOneToCounter()"/>
+<script src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"></script>
+<script src="/week4/amber_ramirez/script.js"/>
+import JSConfetti from 'js-confetti'
+
 function addOneToCounter(){
     document.getElementById("counter").innerText = parseInt(document.getElementById("counter").innerText) + 1
 }
